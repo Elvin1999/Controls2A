ff90a9d3c89e8e4048a2b7ebb78d1e0a99ea383d
Elvin <elvincamalzade00@gmail.com>
2022-10-20 10:33:50 +0400

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.redTrackbar = new System.Windows.Forms.TrackBar();
+            this.greenTrackBar = new System.Windows.Forms.TrackBar();
+            this.blueTrackbar = new System.Windows.Forms.TrackBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eDITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sAVEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sAVEASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXPORTPNGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXPORTJPEXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hIGHQualityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackbar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackbar)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.progressBar1.Location = new System.Drawing.Point(182, 40);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(281, 45);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(470, 40);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(97, 45);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::WindowsFormsApp6.Properties.Resources.myimage;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 270);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(584, 40);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 41);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(330, 119);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 144);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // redTrackbar
+            // 
+            this.redTrackbar.Location = new System.Drawing.Point(461, 223);
+            this.redTrackbar.Maximum = 255;
+            this.redTrackbar.Name = "redTrackbar";
+            this.redTrackbar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.redTrackbar.Size = new System.Drawing.Size(56, 144);
+            this.redTrackbar.TabIndex = 5;
+            this.redTrackbar.Scroll += new System.EventHandler(this.redTrackbar_Scroll);
+            // 
+            // greenTrackBar
+            // 
+            this.greenTrackBar.Location = new System.Drawing.Point(523, 223);
+            this.greenTrackBar.Maximum = 255;
+            this.greenTrackBar.Name = "greenTrackBar";
+            this.greenTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.greenTrackBar.Size = new System.Drawing.Size(56, 144);
+            this.greenTrackBar.TabIndex = 5;
+            this.greenTrackBar.Scroll += new System.EventHandler(this.redTrackbar_Scroll);
+            // 
+            // blueTrackbar
+            // 
+            this.blueTrackbar.Location = new System.Drawing.Point(601, 223);
+            this.blueTrackbar.Maximum = 255;
+            this.blueTrackbar.Name = "blueTrackbar";
+            this.blueTrackbar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.blueTrackbar.Size = new System.Drawing.Size(56, 144);
+            this.blueTrackbar.TabIndex = 5;
+            this.blueTrackbar.Scroll += new System.EventHandler(this.redTrackbar_Scroll);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eDITToolStripMenuItem,
+            this.sAVEToolStripMenuItem,
+            this.sAVEASToolStripMenuItem,
+            this.colorsToolStripMenuItem});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            // 
+            // eDITToolStripMenuItem
+            // 
+            this.eDITToolStripMenuItem.Image = global::WindowsFormsApp6.Properties.Resources.myimage;
+            this.eDITToolStripMenuItem.Name = "eDITToolStripMenuItem";
+            this.eDITToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.eDITToolStripMenuItem.Text = "EDIT";
+            // 
+            // sAVEToolStripMenuItem
+            // 
+            this.sAVEToolStripMenuItem.Name = "sAVEToolStripMenuItem";
+            this.sAVEToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sAVEToolStripMenuItem.Text = "SAVE";
+            // 
+            // sAVEASToolStripMenuItem
+            // 
+            this.sAVEASToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eXPORTPNGToolStripMenuItem,
+            this.eXPORTJPEXToolStripMenuItem});
+            this.sAVEASToolStripMenuItem.Name = "sAVEASToolStripMenuItem";
+            this.sAVEASToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sAVEASToolStripMenuItem.Text = "SAVE AS";
+            // 
+            // eXPORTPNGToolStripMenuItem
+            // 
+            this.eXPORTPNGToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hIGHQualityToolStripMenuItem});
+            this.eXPORTPNGToolStripMenuItem.Name = "eXPORTPNGToolStripMenuItem";
+            this.eXPORTPNGToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.eXPORTPNGToolStripMenuItem.Text = "EXPORT PNG";
+            // 
+            // eXPORTJPEXToolStripMenuItem
+            // 
+            this.eXPORTJPEXToolStripMenuItem.Name = "eXPORTJPEXToolStripMenuItem";
+            this.eXPORTJPEXToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.eXPORTJPEXToolStripMenuItem.Text = "EXPORT JPEX";
+            // 
+            // hIGHQualityToolStripMenuItem
+            // 
+            this.hIGHQualityToolStripMenuItem.Name = "hIGHQualityToolStripMenuItem";
+            this.hIGHQualityToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hIGHQualityToolStripMenuItem.Text = "HIGH Quality";
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem,
+            this.greenToolStripMenuItem});
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.colorsToolStripMenuItem.Text = "Colors";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.G)));
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.blueTrackbar);
+            this.Controls.Add(this.greenTrackBar);
+            this.Controls.Add(this.redTrackbar);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redTrackbar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueTrackbar)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar redTrackbar;
+        private System.Windows.Forms.TrackBar greenTrackBar;
+        private System.Windows.Forms.TrackBar blueTrackbar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eDITToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sAVEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sAVEASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXPORTPNGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hIGHQualityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXPORTJPEXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            this.BackColor = Color.FromArgb(0, 0, 0);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 5;
+            if (progressBar1.Value >= progressBar1.Maximum)
+            {
+                timer.Stop();
+                MessageBox.Show("Downloading ended");
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(int.Parse(numericUpDown1.Value.ToString()), int.Parse(numericUpDown1.Value.ToString()));
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int width = 50;
+            int height = 50;
+            pictureBox1.Size = new Size(width * trackBar1.Value, height * trackBar1.Value);
+        }
+
+        private void redTrackbar_Scroll(object sender, EventArgs e)
+        {
+            int red = redTrackbar.Value;
+            int green = greenTrackBar.Value;
+            int blue = blueTrackbar.Value;
+
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+    }
+}
